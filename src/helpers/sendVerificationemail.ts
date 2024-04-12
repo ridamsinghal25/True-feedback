2d735b185892a99f38b913147759263bbef1dda1
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-12 21:12:42 +0530

    add functionality to send verification email using resend to the codebase

---
+++ src/helpers/sendVerificationemail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerifyEmail from "../../emails/VerifyEmailTemplate";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function SendVerification(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Verification Code",
+      react: VerifyEmail({ username, verifyCode }),
+    });
+
+    return { success: true, message: "Verification email sent successfully." };
+  } catch (Emailerror) {
+    console.log("Error occured while sending verification email: ", Emailerror);
+    return { success: false, message: "Failed to send verification email." };
+  }
+}
