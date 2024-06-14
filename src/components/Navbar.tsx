5abe37a10fd8a795f7fd68f70ff49747d28698f4
ridamsinghal25 <manusinghal84@gmail.com>
2024-06-14 14:11:06 +0530

    refactor navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,33 +13,47 @@ function Navbar() {
 
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
-          True Feedback
-        </a>
-        {session ? (
-          <>
-            <span className="mr-4">
-              Welcome, {user?.username || user?.email}
-            </span>
-            <Button
-              className="w-full md:w-auto bg-slate-100 text-black"
-              variant="outline"
-              onClick={() => signOut()}
-            >
-              Logout
-            </Button>
-          </>
-        ) : (
-          <Link href="/sign-in">
-            <Button
-              className="w-full md:w-auto bg-slate-100 text-black"
-              variant={"outline"}
-            >
-              Login
-            </Button>
-          </Link>
-        )}
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div>
+          <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+            True Feedback
+          </a>
+        </div>
+        <div className="flex gap-2">
+          {!session && (
+            <Link href="/sign-up" className="flex justify-end">
+              <Button
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant={"outline"}
+              >
+                Sign up
+              </Button>
+            </Link>
+          )}
+          {session ? (
+            <>
+              <span className="mr-4">
+                Welcome, {user?.username || user?.email}
+              </span>
+              <Button
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant="outline"
+                onClick={() => signOut()}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Link href="/sign-in">
+              <Button
+                className="w-full md:w-auto bg-slate-100 text-black"
+                variant={"outline"}
+              >
+                Login
+              </Button>
+            </Link>
+          )}
+        </div>
       </div>
     </nav>
   );
